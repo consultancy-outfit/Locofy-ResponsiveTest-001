794c3ad0896cf24cbaca223bebb8ea410b08c0b4
aprfscem1802007 <mkashif6619@gmail.com>
2025-07-05 09:31:57 +0500

    build errors

--- src/app/(pages)/performa-template-wtd-2025-performa-template-wtd-2025/page.tsx
+++ src/app/(pages)/performa-template-wtd-2025-performa-template-wtd-2025/page.tsx
@@ -9,12 +9,7 @@ const PerformaTemplateWtd2025PerformaTemplateWtd2025Page = () => {
     <AmendmentComparison
       pageTitle={`Apprenticeship Journey / Apprenticeship Documents  / vPerforma Template WTD 2025 Onboarding & Initial Setup/ Performa Template WTD 20255`}
       src={PerformaTemplateWtd2025PerformaTemplateWtd2025Image}
-<<<<<<< HEAD
      backRoute="/onboarding-initial-setup"
-=======
-      backRoute="/onboarding-initial-setup"
-      amendmentButtonRoute="/onboarding-initial-setup"
->>>>>>> b99170a9d0e761e1c36ea062c9e5ab547125c052
     />
   );
 };
