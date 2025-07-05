b99170a9d0e761e1c36ea062c9e5ab547125c052
Shahbaz Mughal <shahbazmughal968@gmail.com>
2025-07-05 07:49:44 +0500

    #comment

--- src/app/(pages)/appendix/page.tsx
+++ src/app/(pages)/appendix/page.tsx
@@ -6,17 +6,10 @@ const routes = ["/table-1-expectations-for-word-reading-entry-levels-1-to-4", "/
 
 export default function AppendixPage() {
   return (
-<<<<<<< HEAD
-    <CommonPage
-      pageTitle={`Apprenticeships / Funding Rules / Apprenticeship funding rules 2024_to_2025 / Appendix`}
-      src={AppendixImage}
-      backRoute="/functional-skills-english-conditions-and-requirements"
-=======
     <FourPageCommon
       src={images}
       pageTitle="Apprenticeships / Funding Rules / Apprenticeship funding rules 2024_to_2025 / Appendix"
       imageLinks={routes}
->>>>>>> 6e0960e788247654a58d8341ed22a7708fe9df9e
     />
   );
 }
